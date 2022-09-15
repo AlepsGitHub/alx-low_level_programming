d2fdee0624b37397e1820d8c06f542435ab63731
Alebiosu Opeyemi <alebiosuopeyemi18@gmail.com>
2022-09-15 05:28:50 -0700

    all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,8 @@
 /**
  *
  * main - program that prints _putchar.
- * main - Entry point
  *
- * Return
+ * Return 0
  *
  *
  */
@@ -15,7 +14,7 @@ int main(void)
 {
 char c[8] = "_putchar";
 int i = 0;
-while (i < 9)
+while (i < 8)
 {
 _putchar(c[i]);
 i++;
