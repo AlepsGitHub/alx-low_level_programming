500bb6eb12ab7c4615551db3b7a2e4c47b8cb07c
Alebiosu Opeyemi <94289861+AlepsGitHub@users.noreply.github.com>
2022-09-21 02:10:44 +0100

    all
    all

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates
- * the value it points to to 98
- * @n: pointer to an int
+ * takes a pointer to an int as parameter and updates
+ * points to 98
+ * n pointer to an int
  * Return: Nothing
  */
 
